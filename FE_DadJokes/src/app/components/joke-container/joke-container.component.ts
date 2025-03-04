9a63b932fae9da146de540b377b86fb10411073b
Ajinkya Jacob <ajinkyajacob@gmail.com>
2025-03-04 10:50:26 +0530

    chore: code cleanup.

--- FE_DadJokes/src/app/components/joke-container/joke-container.component.ts
+++ FE_DadJokes/src/app/components/joke-container/joke-container.component.ts
@@ -17,14 +17,16 @@ import { map } from 'rxjs';
   imports: [JokeUI1Component, JokeUI2Component],
   template:`
   @if (isOldUI()) {
-      <app-joke-ui1 [joke]="this.jokeService.joke()"
+      <app-joke-ui1 
+        [joke]="this.jokeService.joke()"
         (onLike)="onLike()"
         (onDislike)="onDisLike()"
         (onRefresh)="onRefresh()"
       />
   
     }@else {
-      <app-joke-ui2 [joke]="this.jokeService.joke()"
+      <app-joke-ui2 
+        [joke]="this.jokeService.joke()"
         (onLike)="onLike()"
         (onDislike)="onDisLike()"
         (onRefresh)="onRefresh()"
@@ -35,11 +37,6 @@ import { map } from 'rxjs';
   styleUrl: './joke-container.component.scss'
 })
 export class JokeContainerComponent {
-
-  jokeUI1 = viewChild(JokeUI1Component)
-  jokeUI2 = viewChild(JokeUI2Component)
-
-  activeUI = computed(() => this.isOldUI() ? this.jokeUI1() : this.jokeUI2())
 
   jokeService = injectJokeService()
 
@@ -61,33 +58,5 @@ export class JokeContainerComponent {
     this.jokeService.refreshJoke().subscribe()
   }
 
-  // constructor() {
-  //   // this.jokeService.getJoke().pipe(catchError((e) => {console.log(e);return EMPTY})).subscribe(v => this.joke.set(v))
-  //   effect(() => {
-  //     const refreshBtn = this.refreshBtn()?.nativeElement;
-  //     const likeBtn = this.likeBtn()?.nativeElement;
-  //     const dislikeBtn = this.dislikeBtn()?.nativeElement;
-  //     if (refreshBtn && likeBtn && dislikeBtn) {
-  //       fromEvent<MouseEvent>([refreshBtn,likeBtn,dislikeBtn], 'mousedown')
-  //         .pipe(
-  //           switchMap((e:any) =>{
-  //             const isLike = e.target.parentElement === likeBtn
-  //             const isdisLike = e.target.parentElement === dislikeBtn
-  //             const type = isdisLike ? 'dislike':'like'
-  //             let count = (this.jokeService.joke()[type] ?? 0) + 1
-  //             console.log(e.target.parentElement === dislikeBtn)
-  //             if( isdisLike || isLike ){
-  //               return this.jokeService.updateLikeDislike(this.jokeService.joke()._id,type, count)
-  //             }
-              
-  //             console.log({e})
-  //             return this.jokeService.refreshJoke()
-  //           }
-  //           ),
-  //         )
-  //         .subscribe();
-  //     }
-  //   });
-  // }
 
 }
